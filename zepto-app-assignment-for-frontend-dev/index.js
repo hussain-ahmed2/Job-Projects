e1f9045fad6908192bffff03417511575bb93475
Hussain Ahmed <hussainahmed.vu@gmail.com>
2024-10-14 18:43:02 +0600

    zepto apps second commit

--- zepto-app-assignment-for-frontend-dev/index.js
+++ zepto-app-assignment-for-frontend-dev/index.js
@@ -1,22 +1,42 @@
-const mainContainer = document.querySelector('main');
+const mainContainer = document.querySelector("main");
 const bookContainer = document.getElementById("book-container");
-const buttonContainer = document.getElementById('btn-container');
+const buttonContainer = document.getElementById("btn-container");
 const searchByTitle = document.getElementById("search-by-title");
-const select = document.getElementById('select');
-const dropdown = document.getElementById('drop-down');
+const select = document.getElementById("select");
+const dropdown = document.getElementById("drop-down");
 const submit = document.getElementById("submit");
 const year = document.getElementById("year");
+const wishedItems = document.getElementById("wished-items");
 
 const date = new Date();
 
 year.innerText = date.getFullYear();
+localStorage.setItem("booksWishList", '[]')
+const wishList = localStorage.getItem("booksWishList")
+  ? JSON.parse(localStorage.getItem("booksWishList"))
+  : [];
+
+function updateWishList(addToWishList, book) {
+  let exists = false;
+  for(let item of wishList) {
+    if (item.id == book.id) {
+      exists = true;
+      break;
+    }
+  }
+  if (!exists) {
+    wishList.push(book);
+    localStorage.setItem("booksWishList", JSON.stringify(wishList));
+  }
+
+  console.log(addToWishList, wishList);
+}
 
 async function fetchApi(URL) {
   const response = await fetch(URL);
   const data = await response.json();
   const arr = [...data.results];
 
-  
   bookContainer.innerHTML = "";
 
   if (!arr.length) {
@@ -25,8 +45,13 @@ async function fetchApi(URL) {
   }
 
   arr.map((book) => {
-    const bookDiv = document.createElement("a");
+    const bookDiv = document.createElement("div");
     bookDiv.classList.add("book");
+
+    const addToWishList = document.createElement("button");
+    addToWishList.textContent = "add to wishlist";
+
+    addToWishList.addEventListener("click", () => updateWishList(addToWishList, book));
 
     const imgContainer = document.createElement("div");
     imgContainer.classList.add("img-container");
@@ -48,13 +73,15 @@ async function fetchApi(URL) {
     const bookGenre = document.createElement("p");
     bookGenre.innerHTML = "Genre: ";
     book.bookshelves.forEach((el, i, arr) => {
-      
-      if (i != arr.length - 1) bookGenre.innerHTML += el.replace('Browsing:', ' ') + " | ";
-      else bookGenre.innerHTML += el.replace('Browsing:', ' ') + ".";
+      if (i != arr.length - 1)
+        bookGenre.innerHTML += el.replace("Browsing:", " ") + " | ";
+      else bookGenre.innerHTML += el.replace("Browsing:", " ") + ".";
     });
 
     const bookId = document.createElement("p");
     bookId.innerText = "ID: " + book.id;
+    bookId.className = "book-id";
+    bookId.appendChild(addToWishList);
 
     const infoContainer = document.createElement("div");
     infoContainer.classList.add("info-container");
@@ -68,30 +95,26 @@ async function fetchApi(URL) {
     bookContainer.appendChild(bookDiv);
   });
 
-  buttonContainer.innerHTML = '';
-  const prevBtn = document.createElement('button');
-  prevBtn.innerText = 'Previous';
-  const nextBtn = document.createElement('button');
-  nextBtn.innerText = 'Next';
+  buttonContainer.innerHTML = "";
+  const prevBtn = document.createElement("button");
+  prevBtn.innerText = "Previous";
+  const nextBtn = document.createElement("button");
+  nextBtn.innerText = "Next";
+
+  buttonContainer.appendChild(prevBtn);
+  buttonContainer.appendChild(nextBtn);
+  mainContainer.appendChild(buttonContainer);
 
   prevBtn.disabled = data.previous == null ? true : false;
   nextBtn.disabled = data.next == null ? true : false;
 
-  prevBtn.addEventListener('click', () => {
-    console.log(data.previous);
-    
+  prevBtn.addEventListener("click", () => {
     fetchApi(data.previous);
   });
 
-  nextBtn.addEventListener('click', () => {
-    console.log(data.next);
-    
+  nextBtn.addEventListener("click", () => {
     fetchApi(data.next);
   });
-
-  buttonContainer.appendChild(prevBtn);  
-  buttonContainer.appendChild(nextBtn);
-  mainContainer.appendChild(buttonContainer);
 }
 
 const URL = "https://gutendex.com/books/";
@@ -102,18 +125,18 @@ function handleFormSubmit(e) {
   if (searchByTitle.value !== "") {
     const searchURL = `https://gutendex.com/books?search=${searchByTitle.value}`;
     fetchApi(searchURL);
-  } else {
-    fetchApi(URL)
-  }
+  } else fetchApi(URL);
 }
 
 submit.addEventListener("click", handleFormSubmit);
 
-dropdown.addEventListener('change', () => {
-  console.log('selected');
-  
-  if (dropdown.value !== '') {
+dropdown.addEventListener("change", () => {
+  if (dropdown.value !== "") {
     const dropdownURL = `https://gutendex.com/books?topic=${dropdown.value}`;
     fetchApi(dropdownURL);
   } else fetchApi(URL);
+});
+
+wishedItems.addEventListener("click", () => {
+  bookContainer.innerHTML = "";
 });
